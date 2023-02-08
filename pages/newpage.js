49632aa540fe1f609324c7c1999b528de4e911d5
Oliver Speir <oliverspeir9@gmail.com>
2023-02-07 18:10:24 -0700

    lab37

---
+++ pages/newpage.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function NewPage() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+            <h1 className="text-4xl">Testing Link</h1>
+            <Link href="/" className="text-blue-700 hover:text-blue-500">Back to Home</Link>
+        </div>
+  );
+}
