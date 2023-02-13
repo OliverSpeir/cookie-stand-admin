267233370ef98163988c83af560dd318212f61b6
Oliver Speir <oliverspeir9@gmail.com>
2023-02-13 13:10:40 -0700

    bugfix

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,4 +1,4 @@
-export default function Header({ locations }) {
+export default function Footer({ locations }) {
   return (
     <footer className=" p-4 bg-green-300 text-black">
       {locations && <h1 className="ml-0">{locations.length} Locations World Wide</h1>}
